b5d8e0a0f1063beaa2452028928e210cf45ca7d4
ifizmat <ifizmat@gmail.com>
2023-12-02 11:29:16 +0400

    Class Row_Squares.

--- test_turtle02.py
+++ test_turtle02.py
@@ -3,9 +3,6 @@ import turtle
 
 # https://docs-python.ru/tutorial/klassy-jazyke-python/sozdanie-sobstvennyh-tipov-dannyh-struktury/
 
-tr = turtle.Turtle()
-scr = turtle.Screen()
-
 @dataclass
 class Square():
     """Data type Square"""
@@ -19,24 +16,40 @@ class Square():
         # initialization attribute "area"
         self.area = self.size ** 2
 
-
-square1 = Square(0, 0, 20)
-square2 = Square(x = -100, y = -50, size = 60, fill_color = "black")
+class Row_Squares():
+    """Data type Row of Squares"""
+    x: int = 0
+    y: int = 0
+    square: Square = field(default = Square)
+    num_squares: int = 0
+    dist_squares: int = 0
 
 def draw_square(turt, square: Square):
     turt.goto(square.x, square.y)
 
-    tr.fillcolor(square.fill_color)
-    tr.begin_fill()
+    turt.fillcolor(square.fill_color)
+    turt.begin_fill()
     turt.pendown()
     for _ in range(4):
         turt.forward(square.size)
         turt.right(90)
     
-    tr.end_fill()
+    turt.end_fill()
     turt.penup()
 
-draw_square(tr, square1)
-draw_square(tr, square2)
+
+if __name__ == "__main__":
+    tr = turtle.Turtle()
+    scr = turtle.Screen()
+
+    square1 = Square(0, 0, 20)
+    square2 = Square(x = -100, y = -50, size = 60, fill_color = "black")
+
+    scr.setup(1440, 780, 1, 0)
+    tr.hideturtle()
+    tr.speed(0)
+
+    draw_square(tr, square1)
+    draw_square(tr, square2)
 
 turtle.done()
