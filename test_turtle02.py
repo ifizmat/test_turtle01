7eb58591a72e33ac8c265bf67e4a990c81541ad9
ifizmat <ifizmat@gmail.com>
2023-12-03 13:24:55 +0400

    draw_row(), instance of class Row_Squares.

--- test_turtle02.py
+++ test_turtle02.py
@@ -16,15 +16,29 @@ class Square():
         # initialization attribute "area"
         self.area = self.size ** 2
 
+@dataclass
 class Row_Squares():
     """Data type Row of Squares"""
     x: int = 0
     y: int = 0
     square: Square = field(default = Square)
-    num_squares: int = 0
-    dist_squares: int = 0
+    amount: int = 0
+    interval: int = 0
 
-def draw_square(turt, square: Square):
+def draw_row(turt: turtle.Turtle, row: Row_Squares):
+    turt.goto(row.x, row.y)
+    for col in range(row.amount):
+        # square = Square(size = 60, fill_color = "white")
+        if col % 2 == 0:
+            row.square.fill_color = "black"
+        else:
+            row.square.fill_color = "white"
+        
+        row.square.x = row.x + col * (row.interval + row.square.size)
+        row.square.y = 0
+        draw_square(turt, row.square)
+
+def draw_square(turt: turtle.Turtle, square: Square):
     turt.goto(square.x, square.y)
 
     turt.fillcolor(square.fill_color)
@@ -43,7 +57,7 @@ if __name__ == "__main__":
     scr = turtle.Screen()
 
     square1 = Square(0, 0, 20)
-    square2 = Square(x = -100, y = -50, size = 60, fill_color = "black")
+    square2 = Square(x = -100, y = -50, size = 8, fill_color = "white")
 
     scr.setup(1440, 780, 1, 0)
     tr.hideturtle()
@@ -51,5 +65,6 @@ if __name__ == "__main__":
 
     draw_square(tr, square1)
     draw_square(tr, square2)
-
-turtle.done()
+    row = Row_Squares(x = -50, y = 0, square = square2, amount = 15, interval = 2)    
+    draw_row(tr, row)
+    turtle.done()
