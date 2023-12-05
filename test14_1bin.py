2e8b997c2b7de889a36eec95b7c80a43b18bbea2
ifizmat <ifizmat@gmail.com>
2023-12-05 09:18:31 +0400

    Test convert binary string to sequence of binary bits.

---
+++ test14_1bin.py
@@ -0,0 +1,14 @@
+﻿bin_str = b'\xbaxuWf\xd3'
+print(f"bin_str: {bin_str}")
+
+num_hex = bin_str.hex()
+print(f"num_hex: {num_hex}")
+
+num_dec = int(num_hex, 16)
+print(f"num_dec: {num_dec}")
+
+num_bin = bin(num_dec)
+print(f"num_bin: {num_bin}")
+
+num_bin = num_bin[2:]
+print(f"num_bin: {num_bin}")
